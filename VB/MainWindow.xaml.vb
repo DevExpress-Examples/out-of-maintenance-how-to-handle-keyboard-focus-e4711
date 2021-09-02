e392fdc171971d61af0366401462b856e4ba97db
DevExpressExampleBot <fakeEmail>
2021-09-02 16:11:02 +0300

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -6,29 +6,29 @@ Imports DevExpress.Xpf.Core.Native
 Imports System.Windows.Controls
 
 Namespace RichEditTabFocusWpf
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-        Private Sub richEditControl1_PreviewKeyDown(ByVal sender As Object, ByVal e As KeyEventArgs)
-            If (Keyboard.IsKeyDown(Key.LeftShift) OrElse Keyboard.IsKeyDown(Key.RightShift)) AndAlso e.Key = Key.Tab Then
-                Dim tRequest As New TraversalRequest(FocusNavigationDirection.Previous)
-                Dim keyboardFocus As UIElement = TryCast(Keyboard.FocusedElement, UIElement)
-                tRequest.Wrapped = True
-                e.Handled = True
-                DirectCast(e.Source, Control).MoveFocus(tRequest)
-                Return
-            End If
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub richEditControl1_PreviewKeyDown(ByVal sender As Object, ByVal e As KeyEventArgs)
+			If (Keyboard.IsKeyDown(Key.LeftShift) OrElse Keyboard.IsKeyDown(Key.RightShift)) AndAlso e.Key = Key.Tab Then
+				Dim tRequest As New TraversalRequest(FocusNavigationDirection.Previous)
+				Dim keyboardFocus As UIElement = TryCast(Keyboard.FocusedElement, UIElement)
+				tRequest.Wrapped = True
+				e.Handled = True
+				DirectCast(e.Source, Control).MoveFocus(tRequest)
+				Return
+			End If
 
-            If e.Key = Key.Tab Then
-                Dim tRequest As New TraversalRequest(FocusNavigationDirection.Next)
-                Dim keyboardFocus As UIElement = TryCast(Keyboard.FocusedElement, UIElement)
-                tRequest.Wrapped = True
-                e.Handled = True
-                keyboardFocus.MoveFocus(tRequest)
-            End If
-        End Sub
-    End Class
+			If e.Key = Key.Tab Then
+				Dim tRequest As New TraversalRequest(FocusNavigationDirection.Next)
+				Dim keyboardFocus As UIElement = TryCast(Keyboard.FocusedElement, UIElement)
+				tRequest.Wrapped = True
+				e.Handled = True
+				keyboardFocus.MoveFocus(tRequest)
+			End If
+		End Sub
+	End Class
 End Namespace
